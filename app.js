70290e581d6a75c8d5c855f78ae34c4e19557c74
dhanikprakash <dhanikprakash@gmail.com>
2020-08-22 15:59:49 +0100

    post methods

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const bookRouter = express.Router();
 const mongoose = require("mongoose");
 const db = mongoose.connect("mongodb://localhost/bookAPI");
 const Book = require("./models/bookModel");
+const bodyParse = require("body-parser");
+const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParse.json());
 bookRouter.route("/books")
 .get((req, res) => {
    const query = {};
@@ -13,6 +18,27 @@ bookRouter.route("/books")
        query.genre = req.query.genre;
    }
     Book.find(query, (err, books) => {
+        if(err){
+           return  res.send(err);
+        }
+
+           return res.json(books);
+
+    });
+});
+bookRouter.route("/books")
+.post((req,res) => {
+    const book = new Book(req.body);
+    book.save();
+    return res.status(201).json(book);
+});
+bookRouter.route("/books/:bookId")
+.get((req, res) => {
+   const query = {};
+   if(req.query.genre){
+       query.genre = req.query.genre;
+   }
+    Book.findById(req.params.bookId, (err, books) => {
         if(err){
            return  res.send(err);
         }
